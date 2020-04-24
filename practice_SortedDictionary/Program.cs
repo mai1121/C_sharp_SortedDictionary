fde452cc809433a838574e0aff8def881581e10e
mai1121 <maimaiukawa@gmail.com>
2020-04-24 21:05:51 +0900

    first commit

---
+++ practice_SortedDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice_SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
